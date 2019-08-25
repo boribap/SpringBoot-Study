4ff548af746d375c23f5e847f8451f90540af757
boribap <bsww201@naver.com>
2019-08-25 10:59:06 +0900

    스웨거 적용

--- board/src/main/java/board/board/dto/BoardFileDto.java
+++ board/src/main/java/board/board/dto/BoardFileDto.java
@@ -1,12 +1,20 @@
 package board.board.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(value="BoardFileDto : 게시글 첨부파일", description="게시글 첨부파일")
 @Data
 public class BoardFileDto {
+	@ApiModelProperty(value="첨부파일 번호")
 	private int idx;
+	@ApiModelProperty(value="게시글 번호")
 	private int boardIdx;
+	@ApiModelProperty(value="원본 파일 이름")
 	private String originalFileName;
+	@ApiModelProperty(value="파일 저장 경로")
 	private String storedFilePath;
+	@ApiModelProperty(value="첨부파일 크기")
 	private long fileSize;
 }
